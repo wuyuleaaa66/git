cf53f5f09a887c403e5b7bc151df0ef99e2ea0ec
wuyuleaaa66 <19833226392@163.com>
2023-04-25 21:10:54 +0800

    three

---
+++ three.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int a[20][20];
+	int i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		a[i][0]=1;
+		a[i][i]=1;
+	}
+	for(i=2;i<n;i++)
+	   for(j=1;j<i;j++)
+	       a[i][j]=a[i-1][j]+a[i][j-1];
+	for(i=0;i<n;i++){
+	    for(j=0;j<i;j++)
+	      printf("%d ",a[i][j]);
+	    printf("\n");
+    }
+    return 0;
+} 
